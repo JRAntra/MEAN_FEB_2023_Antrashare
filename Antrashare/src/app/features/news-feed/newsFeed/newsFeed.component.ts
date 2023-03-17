93327081bb6779c5911816d5a432394aef0cf821
Yusheng Zhang <yusheng.zhang@antra.com>
2023-03-17 16:56:42 -0400

    initializing Angular project

---
+++ Antrashare/src/app/features/news-feed/newsFeed/newsFeed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newsFeed',
+  templateUrl: './newsFeed.component.html',
+  styleUrls: ['./newsFeed.component.css']
+})
+export class NewsFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
