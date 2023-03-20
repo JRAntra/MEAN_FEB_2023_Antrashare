ef17b90f1abc440566795ab0d914c24b2828053e
Alex C <cxz980314@gmail.com>
2023-03-20 18:53:13 -0400

    Change setting-page layout

--- AntraShare/src/app/app.module.ts
+++ AntraShare/src/app/app.module.ts
@@ -4,23 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsfeedModule } from './feature/newsfeed/newsfeed.module';
-
 import { ProfileModule } from './feature/profile/profile.module';
 import { SettingPageModule } from './feature/setting-page.module';
-
-// import { LoginComponent } from './feature/login/login/login.component';
-// import { RegisterComponent } from './feature/register/register/register.component';
-// import { AngularMaterialModule } from './share/angular-material/angular-material.module';
-
 import { NavbarComponent } from './feature/navbar/navbar.component';
 import { AdminModule } from './feature/admin/admin.module';
 import { LoginModule } from './feature/login/login.module';
 import { RegisterModule } from './feature/register/register.module';
 
 @NgModule({
-  declarations: [AppComponent, 
-   NavbarComponent
-  ],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -29,7 +21,7 @@ import { RegisterModule } from './feature/register/register.module';
     SettingPageModule,
     NewsfeedModule,
     LoginModule,
-    RegisterModule
+    RegisterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
