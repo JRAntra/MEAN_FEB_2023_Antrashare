9659989ecbf80583b86694b4c05ce50f4139a4eb
= <Manas09rai@gmail.com>
2023-03-20 01:46:47 -0700

    added minor corrections

--- Antrashare/src/app/shared/app.module.ts
+++ Antrashare/src/app/shared/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from '../features/login/login.module';
 import { MyProfileModule } from '../features/my-profile/my-profile.module';
+import { NewsfeedComponent } from '../features/newsfeed/newsfeed.component';
+import { RegisterComponent } from '../features/register/register.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { MyProfileModule } from '../features/my-profile/my-profile.module';
     BrowserModule,
     AppRoutingModule,
     LoginModule,
-    MyProfileModule
+    MyProfileModule,
+    NewsfeedComponent,
+    RegisterComponent
+
   ],
   providers: [],
   bootstrap: [AppComponent]
