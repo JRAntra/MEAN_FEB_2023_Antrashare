99c2a6d4c65c1f21efc6f2f6f5ea76f85ff7d487
zhaozhihan88 <zhao.zhiha@northeastern.edu>
2023-03-20 08:04:14 -0700

    Updating the routing things

--- Antrashare/src/app/app-routing.module.ts
+++ Antrashare/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './features/admin/admin.component';
+import { SettingsComponent } from './features/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin', component: AdminComponent},
+  {path: 'settings', component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
