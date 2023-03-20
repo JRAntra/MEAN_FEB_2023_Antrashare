868eef17b161df3aa45ad447280478ef973f72fb
yutifan <yoki.yutifan@gmail.com>
2023-03-20 11:13:41 -0400

    update profile feature

--- Antrashare/src/app/features/my-profile/my-profile.module.ts
+++ Antrashare/src/app/features/my-profile/my-profile.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProfileComponent } from './my-profile.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,6 +16,10 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   imports: [
     CommonModule,
+    MatExpansionModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FormsModule,
     MatFormFieldModule,
     MatSelectModule,
     MatIconModule,
