7eebd1286e75134eb81c798af5478197c2104b80
Sixiang Miao <sixiangm@usc.edu>
2023-03-20 02:22:13 -0700

    add admin and Setting

--- Antrashare/src/app/shared/app.module.ts
+++ Antrashare/src/app/shared/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from '../features/login/login.module';
 import { MyProfileModule } from '../features/my-profile/my-profile.module';
+import { SettingModule} from "../features/setting/setting.module";
+import {AdminModule} from "../features/admin/admin.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LoginModule,
-    MyProfileModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        LoginModule,
+        MyProfileModule,
+        SettingModule,
+        AdminModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
