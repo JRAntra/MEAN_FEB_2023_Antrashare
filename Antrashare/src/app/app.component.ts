a394a32c1ce89236d1a7c2301c0e846d1ab22550
zhaozhihan88 <zhao.zhiha@northeastern.edu>
2023-03-20 19:21:43 -0700

    Create Navigate Bar and adjust routing system

--- Antrashare/src/app/app.component.ts
+++ Antrashare/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  constructor(){
+
+  }
   title = 'Antrashare';
 }
--- Antrashare/src/app/app.module.ts
+++ Antrashare/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+
 
 import { MyProfileModule } from './features/my-profile/my-profile.module';
 import { NewsFeedModule } from './features/news-feed/news-feed.module';
@@ -12,22 +12,31 @@ import { AdminModule } from './features/admin/admin.module';
 import { SettingsModule } from './features/settings/settings.module';
 import { LoginModule } from './features/login/login.module';
 import { RegisterModule } from './features/register/register.module';
+import { NavbarComponent } from './features/navbar/navbar.component';
+
+import {ButtonModule} from 'primeng/button';
+import {SelectButtonModule} from 'primeng/selectbutton';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     MyProfileModule,
     NewsFeedModule,
-    FormsModule,
     AdminModule,
     SettingsModule,
     LoginModule,
     RegisterModule,
+    ButtonModule,
+    SelectButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
