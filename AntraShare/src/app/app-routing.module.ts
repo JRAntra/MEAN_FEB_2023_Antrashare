b211594029c0c157a73e467e2100323b277f3acd
Bhim Tamang <ngardenozzy@gmail.com>
2023-03-21 00:47:15 -0500

    routing added to no accounts, and theme color chaged to match the nav bar

--- AntraShare/src/app/app-routing.module.ts
+++ AntraShare/src/app/app-routing.module.ts
@@ -6,20 +6,18 @@ import { ProfileComponent } from './feature/profile/profile.component';
 import { NewsfeedComponent } from './feature/newsfeed/newsfeed.component';
 import { SettingPageComponent } from './feature/setting-page/setting-page.component';
 import { LoginComponent } from './feature/login/login/login.component';
+import { RegisterComponent } from './feature/register/register/register.component';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent},
-  { path: 'news', component: NewsfeedComponent},
-  { path: 'setting', component: SettingPageComponent},
-  { path: 'logIn', component: LoginComponent}
+  { path: 'profile', component: ProfileComponent },
+  { path: 'news', component: NewsfeedComponent },
+  { path: 'setting', component: SettingPageComponent },
+  { path: 'logIn', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule
-  ]
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- AntraShare/src/app/feature/login/login/login.component.ts
+++ AntraShare/src/app/feature/login/login/login.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { 
-  
+  signUp() {
+    this.router.navigate(['/register']);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
