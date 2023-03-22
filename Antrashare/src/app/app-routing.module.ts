a941b7dc4554509015f1a26ddd3c1c64ddd88bd3
Yusheng Zhang <yusheng.zhang@antra.com>
2023-03-22 11:13:46 -0400

    add basic routing

--- Antrashare/src/app/app-routing.module.ts
+++ Antrashare/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { NewsFeedComponent } from './features/news-feed/newsFeed/newsFeed.component';
+import { AdminComponent } from './features/admin/admin.component';
 const routes: Routes = [
+  //localhost:4200/newsfeed
+  {path:"newsfeed",component:NewsFeedComponent},
+  {path:"admin", component:AdminComponent}
+
   
 ];
 
