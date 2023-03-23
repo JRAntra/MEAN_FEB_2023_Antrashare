96aaaca55c024a96fe0150e9518306796e712942
louisleedev <bj.louislee@gmail.com>
2023-03-23 10:51:54 -0700

    feature of adding and deleting user in admin

--- AntraShare/src/app/feature/admin/admin/admin.component.ts
+++ AntraShare/src/app/feature/admin/admin/admin.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  userTags: any[] = [
+    {id: 1, userName: 'User Tag 1'},
+    {id: 2, userName: 'User Tag 2'},
+    {id: 3, userName: 'User Tag 3'}
+  ];
+
+  count: number = 4;
 
   constructor() { }
+
+  onClickEventDelete(idDelete: any){
+    this.userTags = this.userTags.filter(userTag => (userTag.id != idDelete))
+  }
+
+  onClickEventAdd(){
+    this.userTags.push({id: this.count, userName: "User Tag " + this.count})
+    this.count ++;
+  }
 
   ngOnInit(): void {
   }
