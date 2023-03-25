c50ea87f6327277bbfeb5de8de21037ec83db6a3
Dan Nguyen <nguyendq92@gmail.com>
2023-03-25 13:31:45 -0700

    finished newsfeed service

--- AntraShare/src/app/feature/newsfeed/welcome.directive.ts
+++ AntraShare/src/app/feature/newsfeed/welcome.directive.ts
@@ -1,15 +1,20 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
-
+import { Directive, HostBinding, HostListener, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 @Directive({
   selector: '[appWelcome]',
 })
-export class WelcomeDirective {
+export class WelcomeDirective implements OnInit {
   constructor() {}
   @HostBinding('style.visibility') visibility: string = 'visible';
 
-  @HostListener('window:load') onLoad() {
+  ngOnInit() {
     setTimeout(() => {
       this.visibility = 'hidden';
-    }, 5000);
+      environment.isWelcomeMessage = false;
+    }, 100);
   }
+
+  // @HostListener('window:load') onLoad() {
+
+  // }
 }
