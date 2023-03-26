6c4055cf12695ed541469f6e3bcb2ed3d131627d
Alex C <cxz980314@gmail.com>
2023-03-25 23:29:13 -0400

    commit

--- AntraShare/src/app/core/authorization/authorization.service.ts
+++ AntraShare/src/app/core/authorization/authorization.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthorizationService {
-
+  private users: any[] = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+    { username: 'user3', password: 'password3' }
+  ];
   constructor() { }
+
+
+  login(username: string, password: string): boolean {
+    const user = this.users.find(u => u.username === username && u.password === password);
+    if (user) {
+      // store user details in local storage or session storage if required
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
