035b97c568d2dbd9143a78606ac5c6aa447f9665
Alex C <cxz980314@gmail.com>
2023-03-25 23:36:41 -0400

    Adding authorization for login page

--- AntraShare/src/app/feature/login/login/login.component.ts
+++ AntraShare/src/app/feature/login/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgModel } from '@angular/forms';
 import { environment } from 'src/environments/environment';
+import { AuthorizationService } from 'src/app/core/authorization/authorization.service';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,20 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  userName: string = 'ngarden1969';
+  password: string = '';
+  showPassword: boolean = false; //password toggle
+  users: string[] = [];
+  isChatBox: boolean = false;
+
+  constructor(private router: Router, private auth: AuthorizationService) { }
+
+  ngOnInit(): void { }
 
   signUp() {
     this.router.navigate(['/register']);
   }
 
-  userName: string = 'ngarden1969';
-  //password toggle
-  showPassword: boolean = false;
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
   }
@@ -28,24 +34,37 @@ export class LoginComponent implements OnInit {
     this.userName = (event.target as HTMLInputElement).value;
     console.log(this.userName);
   }
-  users: string[] = [];
+
   //on change event, pushes the input value into the array and logs it
   pushToArray(event: Event) {
     this.userName = (event.target as HTMLInputElement).value;
   }
 
-  ngOnInit(): void {}
+  //logs the input on key up event
+  OntypePassword(event: Event) {
+    this.password = (event.target as HTMLInputElement).value;
+    console.log(this.password);
+  }
+
+  //on change event, pushes the input value into the array and logs it
+  pushToPasswordArray(event: Event) {
+    this.password = (event.target as HTMLInputElement).value;
+  }
+
 
   user = {
     isValid: true,
   };
+
   loginClickHandler() {
-    if (this.user.isValid) {
+    const isAuthenticated = this.auth.login(this.userName, this.password);
+    if (isAuthenticated) {
       this.router.navigate(['newsfeed']);
       environment.isWelcomeMessage = false; //to remove welcomeMessage
+    } else {
+      window.alert('wrong');
     }
   }
-  isChatBox: boolean = false;
 
   //need help event handler
   needHelp() {
