245125c2422c880468ea033e0fc6351e469eeaed
louisleedev <bj.louislee@gmail.com>
2023-03-25 20:20:42 -0700

    First Version of FEB23-15

--- AntraShare/src/app/app.module.ts
+++ AntraShare/src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsfeedModule } from './feature/newsfeed/newsfeed.module';
-
 import { ProfileModule } from './feature/profile/profile.module';
 import { SettingPageModule } from './feature/setting-page/setting-page.module';
 import { RegisterModule } from './feature/register/register.module';
-
 import { LoginModule } from './feature/login/login.module';
 import { NavbarModule } from './feature/navbar/navbar.module';
 import { AngularMaterialModule } from './share/angular-material/angular-material.module';
 import { AdminModule } from './feature/admin/admin.module';
+import { UserService } from './core/service/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ProfileModule,
     SettingPageModule,
@@ -29,7 +29,7 @@ import { AdminModule } from './feature/admin/admin.module';
     RegisterModule,
   ],
   exports: [],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- AntraShare/src/app/feature/admin/admin/admin.component.ts
+++ AntraShare/src/app/feature/admin/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/service/user.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,32 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  userTags: any[] = [
-    {id: 1, userName: 'User Tag 1', info: "this is 1/100"},
-    {id: 2, userName: 'User Tag 2', info: "this is 2/100"},
-    {id: 3, userName: 'User Tag 3', info: "this is 3/100"}
-  ];
+  userTags: any[] = [];
 
-  userContent: any[] = []
+  userContent: any;
 
-  count: number = 4;
+  // count: number = 4;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   onClickEventDelete(idDelete: any){
-    this.userTags = this.userTags.filter(userTag => (userTag.id != idDelete))
+    this.userTags = this.userTags.filter(userTag => (userTag._id != idDelete))
   }
 
-  onClickEventAdd(){
-    this.userTags.push({id: this.count, userName: "User Tag " + this.count, info: "this is " + this.count + "/100"})
-    this.count ++;
-  }
+  // onClickEventAdd(){
+  //   this.userTags.push({id: this.count, userName: "User Tag " + this.count, info: "this is " + this.count + "/100"})
+  //   this.count ++;
+  // }
 
   onClickUser(id: number){
-    this.userContent = this.userTags.find(user => user.id === id).info
+    this.userContent = this.userTags.find(user => user._id === id)
   }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => {this.userTags = users})
   }
 
 }
