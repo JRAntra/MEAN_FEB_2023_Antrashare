834c4c27e622aef0debb282d472a4d9adefc56c1
louisleedev <bj.louislee@gmail.com>
2023-03-27 02:24:37 -0700

    22

--- AntraShare/src/app/feature/admin/admin/admin.component.ts
+++ AntraShare/src/app/feature/admin/admin/admin.component.ts
@@ -7,11 +7,9 @@ import { UserService } from 'src/app/core/service/user.service';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  userTags: any[] = [
-    {id: 1, userName: 'User Tag 1'},
-    {id: 2, userName: 'User Tag 2'},
-    {id: 3, userName: 'User Tag 3'}
-  ];
+  userTags: any[] = [];
+
+  userContent: any;
 
   // count: number = 4;
 
@@ -21,13 +19,13 @@ export class AdminComponent implements OnInit {
     this.userTags = this.userTags.filter(userTag => (userTag._id != idDelete))
   }
 
-  onClickEventAdd(){
-    this.userTags.push({id: this.count, userName: "User Tag " + this.count})
-    this.count ++;
-  }
+  // onClickEventAdd(){
+  //   this.userTags.push({id: this.count, userName: "User Tag " + this.count, info: "this is " + this.count + "/100"})
+  //   this.count ++;
+  // }
 
   onClickUser(id: number){
-    this.userContent = this.userTags.find(user => user.id === id).info
+    this.userContent = this.userTags.find(user => user._id === id)
   }
 
   ngOnInit(): void {
@@ -35,3 +33,4 @@ export class AdminComponent implements OnInit {
   }
 
 }
+ 
