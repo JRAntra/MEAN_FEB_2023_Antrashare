9978a33d4ea25ae6727ac8e0b31de3f372835cdd
Sixiang Miao <sixiangm@usc.edu>
2023-03-30 21:03:11 -0700

    fix likelist bugs

--- Antrashare/src/app/features/like-list/likelist.component.ts
+++ Antrashare/src/app/features/like-list/likelist.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { LikelistService } from 'src/app/core/services/likelist/likelist.service';
 import { Subscription } from 'rxjs';
 import { DisplayCharPipe } from 'src/app/shared/display-char.pipe';
+import {MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
   selector: 'app-likelist',
   templateUrl: './likelist.component.html',
   styleUrls: ['./likelist.component.sass'],
-  providers: [DisplayCharPipe]
+  providers: [DisplayCharPipe, MatMenuTrigger]
+
 })
 export class LikeListComponent implements OnInit {
-
+  @ViewChild(MatMenuTrigger) trigger?: MatMenuTrigger;
   likes: string[] = [];
   private likelistSubscription!: Subscription;
 
